d854441cf05ce425889d9bced0d15a7001cafaca
chengcheng <wangcheng_luck@163.com>
2020-08-10 18:48:07 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Cheng';
         $user->email = 'Cheng@163.com';
+        $user->is_admin = true;
         $user->save();
      }
 }
