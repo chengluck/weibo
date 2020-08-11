9dadef52ea3f5c5269d5c29a0c3a9f961fe6f31c
chengcheng <wangcheng_luck@163.com>
2020-08-11 15:38:22 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 // 微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+// 关注者列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+// 粉丝列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
