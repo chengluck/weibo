a5d4315a73befcfd4890223bb8ddcae6cc9b87c3
chengcheng <wangcheng_luck@163.com>
2020-08-11 15:24:18 +0000

    社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -80,7 +80,7 @@ class User extends Authenticatable
         if(! is_array($user_ids)){
             $user_ids = compact('user_ids');
         }
-        $this->followings()->sync($user->ids, false);
+        $this->followings()->sync($user_ids, false);
     }
 
     // 取消关注
